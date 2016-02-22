751f4a0eda5ea93e9124e47d5bb023fba338cd58
GamaSquad <maila_mi@abv.bg>
2016-02-22 10:32:53 +0200

    Added files via upload

---
+++ credentials.php
@@ -0,0 +1,18 @@
+<?php
+
+$users = array(
+	array (
+		'username' => 'rado',
+		'pass' => 'pass123'
+	),
+	array (
+		'username' => 'ceco',
+		'pass' => 'pass123'
+	),
+        array (
+		'username' => 'milan',
+		'pass' => 'pass123'
+	),
+	
+);
+
